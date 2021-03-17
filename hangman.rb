522c29ed260b6480e490594c0d9be9675f778d94
Guillermo <guillermoc93@protonmail.com>
2021-03-17 11:00:30 -0500

    starts game with guesses and word

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,9 @@
 class Game
-  attr_reader :word
+  attr_reader :guesses, :word
 
   def initialize
     @word = random_word
+    @guesses = 6
   end
 
   def random_word
@@ -11,5 +12,11 @@ class Game
       random = File.readlines('5desk.txt').sample.strip.downcase
     end
     random
+  end
+
+  def start_game
+    puts "Welcome to Hangman."
+    puts "You have #{guesses} guesses remaining."
+    puts '_' * word.length
   end
 end
