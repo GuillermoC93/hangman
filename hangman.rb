985fab7916a606e46aa1f7a9e6b07f971eadcbc8
Guillermo <guillermoc93@protonmail.com>
2021-03-17 12:05:30 -0500

    replaces underscores wth occurence of letter, adds to guessed_letters.

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,11 @@
 class Game
-  attr_reader :guesses, :word
+  attr_reader :guesses, :word, :hidden_word
 
   def initialize
     @word = random_word
     @guesses = 6
+    @hidden_word = '_' * word.length
+    @guessed_letters = []
   end
 
   def random_word
@@ -14,9 +16,35 @@ class Game
     random
   end
 
+  def player_guess
+    loop do
+      guess = gets.chomp.downcase
+      if @guessed_letters.include?(guess)
+        puts 'Please input a different guess.'
+      else
+        return guess
+      end
+    end
+  end
+
   def start_game
-    puts "Welcome to Hangman."
+    puts 'Welcome to Hangman.'
     puts "You have #{guesses} guesses remaining."
-    puts '_' * word.length
+    puts @hidden_word
+    puts 'Guess a letter.'
+    puts guess_check
+  end
+
+  def guess_check
+    guess = player_guess
+    @guessed_letters << guess
+    if word.include?(guess)
+      word.each_char.with_index do |c, idx|
+        if word[idx] == guess
+          @hidden_word[idx] = guess
+        end
+      end
+    end
+    @hidden_word
   end
 end
